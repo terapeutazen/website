07302c3387d4cfd840480cd351a28e1b796d5e2c
Relato Consultoria <relato@MacBook-Pro-de-Haroldo.local>
2024-01-21 16:22:00 -0300

    Buddha background adjusted

--- src/components/parallax/Parallax.jsx
+++ src/components/parallax/Parallax.jsx
@@ -25,7 +25,7 @@ const Parallax = ({ type }) => {
       }}
     >
       <motion.h1 style={{ y: yText }}>
-        {type === "services" ? "Saúde-Bem Estar" : "Terapias Holísticas"}
+        {type === "services" ? "Saúde - Bem Estar" : "Terapias Holísticas"}
       </motion.h1>
       <motion.div className="mountains"></motion.div>
       <motion.div
